7968400cf1402e08b3e1f531e447e258b771d899
Paulo <commonzenpython@gmail.com>
2015-08-18 16:38:52 -0400

    fixes old migration

--- aldryn_grid_foundation/migrations/0003_auto__add_field_gridcolumnfoundation_size_medium.py
+++ aldryn_grid_foundation/migrations/0003_auto__add_field_gridcolumnfoundation_size_medium.py
@@ -2,21 +2,40 @@
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
+from django.db import connection, models
 
 
 class Migration(SchemaMigration):
 
+    old_style = 'cmsplugin_gridcolumnfoundation'
+    new_style = 'aldryn_grid_foundation_gridcolumnfoundation'
+
+    def is_old_style(self):
+        # returns true only if the old style table is present
+        # and the new style one is not
+        table_names = connection.introspection.table_names()
+        is_new_style = self.new_style in table_names
+        return self.old_style in table_names and not is_new_style
+
+    def get_table_name(self):
+        if self.is_old_style():
+            table_name = self.old_style
+        else:
+            table_name = self.new_style
+        return table_name
+
     def forwards(self, orm):
+        table_name = self.get_table_name()
         # Adding field 'GridColumnFoundation.size_medium'
-        db.add_column(u'aldryn_grid_foundation_gridcolumnfoundation', 'size_medium',
+        db.add_column(table_name, 'size_medium',
                       self.gf('django.db.models.fields.IntegerField')(default=0, null=True, blank=True),
                       keep_default=False)
 
 
     def backwards(self, orm):
+        table_name = self.get_table_name()
         # Deleting field 'GridColumnFoundation.size_medium'
-        db.delete_column(u'aldryn_grid_foundation_gridcolumnfoundation', 'size_medium')
+        db.delete_column(table_name, 'size_medium')
 
 
     models = {
